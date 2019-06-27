9d740154a6cbe764c633e6058f870764be382a63
Brian Timar <timarbrian@gmail.com>
2019-06-27 10:40:40 -0400

    timeev stuff

--- qutip_hamiltonians.py
+++ qutip_hamiltonians.py
@@ -2,6 +2,7 @@
 import qutip as qt
 import numpy as np
 from qutip_utils import embed, n
+import itertools as it
 
 def dist1d(i, j, L, bc='open'):
     """Distance between two lattice sites on a 1d chain.
@@ -87,6 +88,60 @@ def heisenberg_1d(param_dict):
         J = J/2
 
     return -J * (1/4) * (sum(pauli_x) + sum(pauli_y) + sum(pauli_z))
+
+
+def get_H_XY_terms(N):
+    """ Returns local terms, without coupling coefficients, that make up the XY hamiltonian.
+        returns: Hx, Hz, HPM
+        Hx = list of pauli X's
+        Hz = list of Pauli Z's
+        HPM = list of sigma_plus * sigma_minus """
+
+    I=qt.tensor([qt.qeye(2)] * N)
+    c=list(it.combinations(range(N), 2))
+    Hz,Hx, HPM=[0 * I for x in range(N)],[0 * I for x in range(N)], [0 * I for x in c]
+    for i in range(N):
+        l=[qt.qeye(2)] * N
+        l[i]=qt.sigmaz()
+        Hz[i]=qt.tensor(l)
+        l[i]=qt.sigmax()
+        Hx[i]=qt.tensor(l)
+    for s in range(len(c)):
+        i, j=c[s]
+        l=[qt.qeye(2)] * N
+        l[i]=qt.sigmap()
+        l[j]=qt.sigmam()
+        HPM[s]=qt.tensor(l)
+        HPM[s]+=HPM[s].dag()
+    return Hx,Hz, HPM
+
+
+def construct_XY_Hamiltonian(N):
+    """Construct a matrix representation of the XY hamiltonian.
+    N: the system size.
+    The interaction strengths are loaded from Jij_10.npy; hence system size can be at most 10"""
+
+    if N > 10:
+        raise ValueError("coupling strengths for N > 10 not provided")
+        
+    __, Hz, HPM = get_H_XY_terms(N)   # returns lists containing the individual terms of the Hamiltonian
+                            # Hz: local sigma_z terms
+                            # HPM: flip flop (between every to sites)
+
+    disorder=[0.]*N  # optional disorder potential (here put to zero)
+
+    ### Construct the Jij matrix
+    Jij = np.load("Jij_10.npy")
+
+    ### Construct the Hamiltonian
+    H = sum([disorder[i]  * h for i,h in enumerate(Hz)], 0) # Local terms
+    c=list(it.combinations(range(N), 2)) # all combinations (but lower triangular)
+    for s, cs in enumerate(c):
+        i, j = cs
+        H+=HPM[s]*(Jij[i,j]+Jij[j,i])
+
+    return H
+
 
 
 hamiltonian_constructors = { 'rydberg': rydberg_hamiltonian_1d, 
